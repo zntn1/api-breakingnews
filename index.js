f05f1b8d44696f8a4483e63c404f21a26064ccbf
zntn1 <zntn.bhz@gmail.com>
2023-06-22 20:44:35 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import connectDatabase from './src/database/db.js'
+import dotenv from 'dotenv'
+
+import userRoute from './src/routes/user.route.js'
+import authRoute from './src/routes/auth.route.js'
+import newsRoute from './src/routes/news.route.js'
+
+
+dotenv.config()
+
+
+const port = process.env.PORT || 3000
+const app = express()
+
+connectDatabase()
+app.use(express.json())
+app.use('/user', userRoute )
+app.use('/auth', authRoute )
+app.use('/news', newsRoute )
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
