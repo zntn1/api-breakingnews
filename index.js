da207b07e3e0f64dcfa03f680e9533357626eafa
zntn1 <zntn.bhz@gmail.com>
2023-06-23 11:05:06 -0300

    refactor: final

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ import userRoute from './src/routes/user.route.js'
 import authRoute from './src/routes/auth.route.js'
 import newsRoute from './src/routes/news.route.js'
 
-
 dotenv.config()
-
 
 const port = process.env.PORT || 3000
 const app = express()
